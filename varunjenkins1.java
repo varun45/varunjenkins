fbd66112315c7f41fcc88ef7266d3586af163f1b
varun Chilakala <56544463+varun45@users.noreply.github.com>
2022-03-28 20:48:10 +0530

    Create varunjenkins1.java

---
+++ varunjenkins1.java
@@ -0,0 +1,17 @@
+public class Addition {
+   public static void main(String[] args) {
+
+      // declare variables
+      int num1, num2, sum;
+
+      // take two numbers
+      num1 = 10;
+      num2 = 20;
+
+      // calculate sum value
+      sum = num1 + num2;
+
+      // display the sum value
+      System.out.println("Sum = " + sum);
+   }
+}
